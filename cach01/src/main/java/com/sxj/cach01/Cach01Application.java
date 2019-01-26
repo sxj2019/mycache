d180839427a20bfc0063a9830072d8ec972925d5
sxj2019 <1010861233@qq.com>
2019-01-26 15:02:22 +0800

    Initial commit

---
+++ cach01/src/main/java/com/sxj/cach01/Cach01Application.java
@@ -0,0 +1,14 @@
+package com.sxj.cach01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cach01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cach01Application.class, args);
+	}
+
+}
+
---
+++ cach02/src/main/java/com/sxj/cach02/Cach02Application.java
@@ -0,0 +1,14 @@
+package com.sxj.cach02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cach02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cach02Application.class, args);
+	}
+
+}
+
